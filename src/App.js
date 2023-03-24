d8acbace2052e6f19750910aa7ca895d69bf525a
hellokkki <akibigbabytape@gmail.com>
2023-03-24 14:46:42 +0300

    'add scss'

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
 
   return (
     <div className="App">
-  
-      <main>
+      <main className='main-content'>
      <div className='select-block'>
      <ChessTimeSetter />
      </div>
